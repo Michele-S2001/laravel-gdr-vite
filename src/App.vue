c4d862d1215b3bc463a88e0166e28788e56b1687
MicheleSerafini <serafini.michele01@gmail.com>
2024-01-18 15:25:50 +0100

    ADD > Initial setup

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+
+export default {
+  data() {
+    return {
+      hello: 'Hello vite'
+    }
+  }
+}
+
+</script>
+
+<template>
+  <h1>{{hello}}</h1>
+</template>
+
+<style lang="scss">
+</style>
