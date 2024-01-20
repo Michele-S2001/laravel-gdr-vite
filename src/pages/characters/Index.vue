d1fbc448ab8d674822ee0546ac853e0bae71bf38
MicheleSerafini <serafini.michele01@gmail.com>
2024-01-20 12:15:50 +0100

    ADD > A filtered search based on which type is selected

--- src/pages/characters/Index.vue
+++ src/pages/characters/Index.vue
@@ -6,82 +6,162 @@ import store from '../../store';
   export default {
     data() {
       return {
-        characters: null
+        characters: null,
+        listOfTypes: null,
+        filteredTypes: [],
+        indexCharactersUrl: `${store.BASE_URL}/characters` 
       }
     },
 
     methods: {
       fetchCharacters() {
         axios
-          .get(`${store.BASE_URL}/characters`)
+          .get(this.indexCharactersUrl, {
+            params: {
+              arrFilteredTypes: this.filteredTypes
+            }
+          })
           .then((res) => {
             this.characters = res.data.results
+          })
+      },
+
+      fetchTypes() {
+        axios
+          .get(`${store.BASE_URL}/types`)
+          .then((res) => {
+            this.listOfTypes = res.data.results
           })
       }
     },
     
     created() {
       this.fetchCharacters();
+      this.fetchTypes();
     },
   }
 </script>
 
 <template>
-  <section class="section">
-    <div class="container">
-      <div class="characters-wrapper">
-        <div class="character-card" v-for="character in characters" :key="character.id">
-          <div class="character-card__header" >
-            <h4>{{ character.name }}</h4>
+  <div class="content-wrapper">
+    <aside class="sidebar">
+      <h5 class="filter-title">Filtra i personaggi</h5>
+      <form class="filter-form" @submit.prevent="fetchCharacters">
+        <div class="checkboxes-wrap">
+          <div class="check-type" v-for="currType in listOfTypes" :key="currType.id">
+            <label :for="currType.slug">{{ currType.name }}</label>
+            <input v-model="filteredTypes" type="checkbox" name="typesSelected" :id="currType.slug" :value="currType.id">
           </div>
-          <div class="character-card__body">
-            <ul class="stats">
-              <li><span>Def: </span>{{ character.def }}</li>
-              <li><span>Speed: </span>{{ character.speed }}</li>
-              <li><span>Hp: </span>{{ character.hp }}</li>
-            </ul>
-          </div>
-          <div class="character-card__footer">
-            <router-link :to="{ name: 'characters.show', params: { id: character.id } }">More</router-link>
+        </div>
+        <input class="filter-submit" type="submit" value="Filtra">
+      </form>
+    </aside>
+    <section class="section main-content">
+      <div class="container">
+        <div class="characters-wrapper">
+          <div class="character-card" v-for="character in characters" :key="character.id">
+            <div class="character-card__header" >
+              <h4>{{ character.name }}</h4>
+            </div>
+            <div class="character-card__body">
+              <ul class="stats">
+                <li><span>Def: </span>{{ character.def }}</li>
+                <li><span>Speed: </span>{{ character.speed }}</li>
+                <li><span>Hp: </span>{{ character.hp }}</li>
+              </ul>
+              <div class="type">Type ==>{{ character.type.name }}</div>
+            </div>
+            <div class="character-card__footer">
+              <router-link :to="{ name: 'characters.show', params: { id: character.id } }">More</router-link>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 @use '../../style/partials/vars' as *;
-.characters-wrapper {
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
-  gap: 30px;
 
-  .character-card {
-    border: 1px solid $black;
-    border-radius: 30px;
+.content-wrapper {
 
-    &__header {
-      background-color: $grey;
-      border-top-right-radius: 30px;
-      border-top-left-radius: 30px;
-      padding: 20px 10px 10px 20px;
-      text-transform: capitalize;
+  display: flex;
+
+  .sidebar {
+    flex-basis: 300px;
+    border-right: 2px solid $black;
+    padding: 10px 5px 10px;
+
+    .filter-title {
+      font-size: 22px;
+      font-weight: 500;
+      margin-bottom: 24px;
     }
 
-    &__body {
-      padding: 10px 20px 10px 20px;
+    .filter-form {
+      
+      .checkboxes-wrap {
+        display: flex;
+        justify-content: space-around;
+        flex-wrap: wrap;
+        column-gap: 20px;
+        row-gap: 10px;
+        margin-bottom: 10px;
+      }
 
-      .stats {
-        & > * {
-          margin-bottom: 10px;
-        }
+      .filter-submit {
+        background-color: $primary;
+        color: $white;
+        line-height: 30px;
+        padding: 0 15px;
+        font-size: 18px;
+        border-radius: 12px;
+        cursor: pointer;
+      }
+      .check-type {
+        display: flex;
+        align-items: center;
+        gap: 5px;
       }
     }
+  }
 
-    &__footer {
-      text-align: center;
-      padding: 10px 0;
+  .main-content {
+
+    flex-grow: 1;
+    .characters-wrapper {
+      display: grid;
+      grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+      gap: 30px;
+    
+      .character-card {
+        border: 1px solid $black;
+        border-radius: 30px;
+    
+        &__header {
+          background-color: $grey;
+          border-top-right-radius: 30px;
+          border-top-left-radius: 30px;
+          padding: 20px 10px 10px 20px;
+          text-transform: capitalize;
+        }
+    
+        &__body {
+          padding: 10px 20px 10px 20px;
+    
+          .stats {
+            & > * {
+              margin-bottom: 10px;
+            }
+          }
+        }
+    
+        &__footer {
+          text-align: center;
+          padding: 10px 0;
+        }
+      }
     }
   }
 }
